65972d067d0f0fa0a970cb195a87764044f20801
Harshasree23 <sreeharsha2358@gmail.com>
2025-02-25 11:14:58 +0530

    Changed the loader

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -1,13 +1,12 @@
+import React from 'react';
 import './Loader.css';
 
-export const Loader = ()=>
-{
-
-    return(
-        <div className="loader">
-            <div className="loader-box1"></div>
-            <div className="loader-box2"></div>
-            <div className="loader-box3"></div>
-        </div>
-    )
-};
+export const Loader = () => {
+  return (
+    <>
+    <div className='loading' >
+        <div class="loader"></div>
+    </div>
+    </>
+  );
+}
